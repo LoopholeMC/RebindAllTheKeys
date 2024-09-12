1821e9a7156930601b79817a6be76d3ee6abca87
Jakob <minenash@protonmail.com>
2024-09-12 02:29:44 -0400

    Wrap Operation

--- src/main/java/com/minenash/rebind_all_the_keys/mixin/HandledScreenMixin.java
+++ src/main/java/com/minenash/rebind_all_the_keys/mixin/HandledScreenMixin.java
@@ -1,5 +1,7 @@
 package com.minenash.rebind_all_the_keys.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.minenash.rebind_all_the_keys.RebindAllTheKeys;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,24 +11,24 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(HandledScreen.class)
 public class HandledScreenMixin {
 
-    @Redirect(method = "mouseReleased", at = @At(value = "INVOKE",target = "Lnet/minecraft/client/gui/screen/ingame/HandledScreen;hasShiftDown()Z"))
-    public boolean rebindShift() {
-        return RebindAllTheKeys.QUICK_MOVE.isDefault() ? HandledScreen.hasShiftDown() : RebindAllTheKeys.isKeybindPressed(RebindAllTheKeys.QUICK_MOVE);
+    @WrapOperation(method = "mouseReleased", at = @At(value = "INVOKE",target = "Lnet/minecraft/client/gui/screen/ingame/HandledScreen;hasShiftDown()Z"))
+    public boolean rebindShift(Operation<Boolean> original) {
+        return RebindAllTheKeys.QUICK_MOVE.isDefault() ? original.call() : RebindAllTheKeys.isKeybindPressed(RebindAllTheKeys.QUICK_MOVE);
     }
 
-    @Redirect(method = "mouseReleased", at = @At(value = "INVOKE",target = "Lnet/minecraft/client/util/InputUtil;isKeyPressed(JI)Z"))
-    public boolean rebindShift2(long _handle, int _code) {
-        return RebindAllTheKeys.QUICK_MOVE.isDefault() ? HandledScreen.hasShiftDown() : RebindAllTheKeys.isKeybindPressed(RebindAllTheKeys.QUICK_MOVE);
+    @WrapOperation(method = "mouseReleased", at = @At(value = "INVOKE",target = "Lnet/minecraft/client/util/InputUtil;isKeyPressed(JI)Z"))
+    public boolean rebindShift2(long handle, int code, Operation<Boolean> original) {
+        return RebindAllTheKeys.QUICK_MOVE.isDefault() ? original.call(handle, code) : RebindAllTheKeys.isKeybindPressed(RebindAllTheKeys.QUICK_MOVE);
     }
 
-    @Redirect(method = "mouseClicked", at = @At(value = "INVOKE",target = "Lnet/minecraft/client/util/InputUtil;isKeyPressed(JI)Z"))
-    public boolean rebindShift3(long _handle, int _code) {
-        return RebindAllTheKeys.QUICK_MOVE.isDefault() ? HandledScreen.hasShiftDown() : RebindAllTheKeys.isKeybindPressed(RebindAllTheKeys.QUICK_MOVE);
+    @WrapOperation(method = "mouseClicked", at = @At(value = "INVOKE",target = "Lnet/minecraft/client/util/InputUtil;isKeyPressed(JI)Z"))
+    public boolean rebindShift3(long handle, int code, Operation<Boolean> original) {
+        return RebindAllTheKeys.QUICK_MOVE.isDefault() ? original.call(handle, code) : RebindAllTheKeys.isKeybindPressed(RebindAllTheKeys.QUICK_MOVE);
     }
 
-    @Redirect(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/HandledScreen;hasControlDown()Z"))
-    public boolean rebindDropStackModifier() {
-        return RebindAllTheKeys.DROP_STACK_MODIFIER.isDefault() ? HandledScreen.hasControlDown() : RebindAllTheKeys.isKeybindPressed(RebindAllTheKeys.DROP_STACK_MODIFIER);
+    @WrapOperation(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/HandledScreen;hasControlDown()Z"))
+    public boolean rebindDropStackModifier(Operation<Boolean> original) {
+        return RebindAllTheKeys.DROP_STACK_MODIFIER.isDefault() ? original.call() : RebindAllTheKeys.isKeybindPressed(RebindAllTheKeys.DROP_STACK_MODIFIER);
     }
 
 }
